4b6994b345fd63aeaf990b64d4c173358e1088cb
danielyuenhx <yuendaniel33@gmail.com>
2022-08-27 00:21:56 +0800

    added code to connect to the db

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const nodemon = require('nodemon');
+const cors = require('cors');
 
 // initialise express app
 const app = express();
 
-// bodyparser to parse requests 
+// bodyparser to parse requests
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 
 app.use(cors());
+
+// environment variables
+const USERNAME = process.env.POSTR_USERNAME;
+const PASSWORD = process.env.POSTR_PASSWORD;
+
+const CONNECTION_URL = `mongodb+srv://${USERNAME}:${PASSWORD}@postr.aznsaak.mongodb.net/?retryWrites=true&w=majority`;
+const PORT = process.env.PORT || 5000;
+
+// mongoose to connect to MongoDB
+// .then runs after successful connection, app listens to the port and runs callback function
+mongoose
+	.connect(CONNECTION_URL, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() =>
+		app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+	)
+	.catch((error) => console.log(error.message));
