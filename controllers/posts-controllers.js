95c877309cad498b28af28cce7d32ea39636d713
danielyuenhx <yuendaniel33@gmail.com>
2022-09-26 22:56:33 +0800

    added delete post functionality and delete UI

--- controllers/posts-controllers.js
+++ controllers/posts-controllers.js
@@ -5,23 +5,39 @@ import PostModel from '../models/posts-model.js';
 // callback functions for routing
 export const getPosts = async (req, res) => {
 	try {
-        const posts = await PostModel.find();
-        res.status(200).json(posts);
+		const posts = await PostModel.find();
+		res.status(200).json(posts);
 	} catch (error) {
 		res.status(404).json({ message: error.message });
 	}
 };
 
 export const createPost = async (req, res) => {
-    // POST requests need access to request body
-    const post = req.body;
-    const newPost = new PostModel(post);
+	// POST requests need access to request body
+	const post = req.body;
+	const newPost = new PostModel(post);
 
-    try {
-        // save to database
-        await newPost.save();
-        res.status(200).json(newPost);
-    } catch (error) {
+	try {
+		// save to database
+		await newPost.save();
+		res.status(200).json(newPost);
+	} catch (error) {
 		res.status(404).json({ message: error.message });
-    }
-}
+	}
+};
+
+export const deletePost = async (req, res) => {
+	const { id: _id } = req.params;
+
+	// check if Post with ID exists
+	if (!mongoose.Types.ObjectId.isValid(_id))
+		return res.status(404).send('No post with that ID.');
+
+	try {
+		// find and delete
+		await PostModel.findByIdAndRemove(_id);
+		res.status(200).json({ message: 'Post deleted successfully.' });
+	} catch (error) {
+		res.status(404).json({ message: error.message });
+	}
+};
--- routes/posts-routes.js
+++ routes/posts-routes.js
@@ -2,11 +2,12 @@
 import express from 'express';
 
 // callback functions stored in controllers folder
-import { getPosts, createPost } from '../controllers/posts-controllers.js';
+import { getPosts, createPost, deletePost } from '../controllers/posts-controllers.js';
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.post('/', createPost);
+router.delete('/:id', deletePost);
 
 export default router;
